6cdafaf379b0f97e9da9d2168f3721124439baaf
cecstar <yocecia@gmail.com>
2016-10-12 15:44:01 +0000

    express assignments from day2

---
+++ using-express/express_server.js
@@ -0,0 +1,31 @@
+var express = require("express");
+var app = express();
+app.set("view engine", "ejs");
+var PORT = process.env.PORT || 8080;
+
+var urlDatabase = {
+  "b2xVn2": "http://www.lighthouselabs.ca",
+  "9sm5xK": "http://www.google.com"
+};
+
+app.get("/", (req, res) => {
+  res.end("Hello!");
+});
+
+app.get("/urls.json", (req, res) => {
+  res.json(urlDatabase);
+});
+
+app.get("/urls", (req, res) => {
+  let templateVars = { urls: urlDatabase };
+  res.render("urls_index", templateVars);
+});
+
+app.get("/urls/:id", (req, res) => {
+  let templateVars = { shortURL: req.params.id };
+  res.render("urls_show", templateVars);
+});
+
+app.listen(PORT, () => {
+  console.log(`Example app listening on port ${PORT}!`);
+});
